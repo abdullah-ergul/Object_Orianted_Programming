55408ab3319b372cfb3c6439a152d37f41edc70a
Abdullah Ergül <abdullahergul23@gmail.com>
2022-10-22 11:44:19 +0300

    Upload From PC

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 import BEng_2_1_NehadTARamaha.week03_Access_Modifiers.BankAccount;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input= new Scanner(System.in);
         BankAccount example = new BankAccount(5960, "Abdullah");
 
         example.withdraw();
