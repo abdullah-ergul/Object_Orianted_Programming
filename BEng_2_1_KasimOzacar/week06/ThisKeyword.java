d0d9a59535a2e6285721dbe54448d34ba79409a0
Abdullah Ergül <abdullahergul23@gmail.com>
2022-11-08 00:19:29 +0300

    Upload From PC

---
+++ BEng_2_1_KasimOzacar/week06/ThisKeyword.java
@@ -0,0 +1,47 @@
+package BEng_2_1_KasimOzacar.week06;
+
+public class ThisKeyword{
+    int x;
+    int y;
+    int height;
+    int lenght;
+
+    public ThisKeyword(){
+        // this.x= 0;
+        // this.y= 0;
+        // this.height= 1;
+        // this.lenght= 1;
+        this(0,0,1,1);
+    }
+
+    public ThisKeyword(int height, int lenght){
+        // this.x= 0;
+        // this.y= 0;
+        // this.height= height;
+        // this.lenght= lenght;
+        this(0,0,height,lenght);
+    }
+
+    public ThisKeyword(int x, int y, int height, int lenght){
+        this.x= x;
+        this.y= y;
+        this.height= height;
+        this.lenght= lenght;
+    }
+
+    public double getArea(){
+        return this.height * this.lenght;
+    }
+
+    // ! MAIN SECTION
+    public static void main(String[] args){
+        ThisKeyword ex1= new ThisKeyword();
+        ThisKeyword ex2= new ThisKeyword(10, 15);
+        ThisKeyword ex3= new ThisKeyword(-5, 5, 10, 15);
+
+        System.out.println(ex1.getArea());
+        System.out.println(ex2.getArea());
+        System.out.println(ex3.getArea());
+    }
+    // ! MAIN SECTION
+}
---
+++ BEng_2_1_KasimOzacar/week06/toStringKeyword.java
@@ -0,0 +1,22 @@
+package BEng_2_1_KasimOzacar.week06;
+
+public class toStringKeyword {
+    int id;
+    String name;
+
+    public toStringKeyword(int id, String name){
+        this.name= name;
+        this.id= id;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
+
+    // ! MAIN SECTION
+    public static void main(String[] args){
+        
+    }
+    // ! MAIN SECTION
+}
