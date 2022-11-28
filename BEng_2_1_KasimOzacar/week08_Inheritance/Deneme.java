8a43ea9c1fd04a7ab9e14da86ca44b988fa8e907
Abdullah Ergül <abdullahergul23@gmail.com>
2022-11-28 12:36:36 +0300

    Create Deneme.java

---
+++ BEng_2_1_KasimOzacar/week08_Inheritance/Deneme.java
@@ -0,0 +1,25 @@
+package BEng_2_1_KasimOzacar.week08_Inheritance;
+
+class Animal {
+    String color;
+    int age;
+}
+
+class Dog extends Animal {
+
+}
+
+class Cat extends Animal {
+    public void meaw(){
+        System.out.println("MEOWW");
+    }
+}
+
+
+public class Deneme {
+    // ! MAIN SECTION
+    public static void main(String[] args) {
+        
+    }
+    // ! MAIN SECTION
+}
