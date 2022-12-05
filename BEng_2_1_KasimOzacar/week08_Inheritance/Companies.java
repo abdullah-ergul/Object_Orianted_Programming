d8ccf95d851fff81622ac4b5d54ba90710798105
Abdullah Ergül <abdullahergul23@gmail.com>
2022-12-05 09:48:04 +0300

    Update Companies.java

--- BEng_2_1_KasimOzacar/week08_Inheritance/Companies.java
+++ BEng_2_1_KasimOzacar/week08_Inheritance/Companies.java
@@ -2,7 +2,7 @@ package BEng_2_1_KasimOzacar.week08_Inheritance;
 
 
 class Company {
-    private String name;
+    private String name= "Company Name";
 
     protected String getAdress() {
         return "Company's Adress";
@@ -11,9 +11,21 @@ class Company {
     public void print() {
         System.out.println("Hi!");
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
 
 class AkinSoft extends Company {
+    public AkinSoft() {
+        super.setName("Akinsoft Company");
+    }
+
     @Override
     public String getAdress() {
         return "AkinSoft's Adress";
@@ -21,6 +33,10 @@ class AkinSoft extends Company {
 }
 
 class GoogleCompany extends Company {
+    public GoogleCompany() {
+        super.setName("Google Company");
+    }
+
     @Override
     protected String getAdress() {
         return "Google Company's Adress";
@@ -37,6 +53,10 @@ class GoogleCompany extends Company {
 }
 
 class MicrosoftCompany extends Company {
+    public MicrosoftCompany() {
+        super.setName("Microsoft Company");
+    }
+
     @Override
     public String getAdress() {
         return "Microsoft Company's Adress";
