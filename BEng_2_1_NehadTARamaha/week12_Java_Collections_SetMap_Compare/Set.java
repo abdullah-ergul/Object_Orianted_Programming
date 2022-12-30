d5f5b82f30eb9d6084e2c18f7e63aed2901ec446
Abdullah Ergül <abdullahergul23@gmail.com>
2022-12-30 10:31:47 +0300

    Update Set.java

--- BEng_2_1_NehadTARamaha/week12_Java_Collections_SetMap_Compare/Set.java
+++ BEng_2_1_NehadTARamaha/week12_Java_Collections_SetMap_Compare/Set.java
@@ -1,5 +1,6 @@
 package BEng_2_1_NehadTARamaha.week12_Java_Collections_SetMap_Compare;
 
+import java.util.AbstractSet;
 import java.util.HashSet;
 import java.util.TreeSet;
 
@@ -7,10 +8,28 @@ import java.util.TreeSet;
 public class Set {
     // ! MAIN SECTION
     public static void main(String[] args) {
-        HashSet<String> hSet = new HashSet<>();
-
-        TreeSet<Integer> tSet = new TreeSet<>();
         
+        AbstractSet<String> hSet = new HashSet<>();
+        
+        hSet.add("Ted");
+        hSet.add("Barney");
+        hSet.add("Robin");
+        hSet.add("Marshall");
+        hSet.add("Lilly");
+        hSet.add("Barney");  // Is desn't added
+
+        System.out.println(hSet);
+
+
+        AbstractSet<Integer> tSet = new TreeSet<>();
+
+        tSet.add(18);
+        tSet.add(93);
+        tSet.add(7);
+        tSet.add(23);
+        tSet.add(21);
+
+        System.out.println(tSet);
     }
     // ! MAIN SECTION
 }
