b2fef644c52f6d024b03940a74090daff0d72771
Abdullah Ergül <abdullahergul23@gmail.com>
2023-01-04 20:49:35 +0300

    Upload New Files

---
+++ BEng_2_1_KasimOzacar/week13_FileOperators_/GraphicUserInterface/src/StudentRegisterForm.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class StudentRegisterForm extends JFrame {
+    private JButton saveButton;
+    private JTextField textField1;
+    private JLabel fName;
+
+    StudentRegisterForm() {
+        this.setTitle("Register Student");
+        this.setSize(400,300);
+        this.add(f);
+    }
+}
